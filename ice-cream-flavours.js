12dd2335022bf4679d0e88389ba38748c5b05358
Adam Dipinto <adam.dipinto@gmail.com>
2016-09-25 22:57:52 +0000

    Adds conditional exercises

---
+++ ice-cream-flavours.js
@@ -0,0 +1,14 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours.length);
+
+console.log(iceCreamFlavours[0]);
+
+var length = iceCreamFlavours.length;
+
+console.log(length + 1);
+
+console.log("Number of flavours " + iceCreamFlavours.length);
